1f36fc708683cfed56deb68298db1598e5198ed3
A-PachecoT <andre.pacheco.t@uni.pe>
2026-06-16 21:40:52 -0500

    fuera comentarios de WHY

--- containers/hashtable.h
+++ containers/hashtable.h
@@ -11,8 +11,6 @@
 
 using namespace std;
 
-// KVPair<Key, Value> — value_type del HashTable.
-// El AVL ordena por key porque operator< mira solo key.
 template <typename Key, typename Value>
 class KVPair {
 public:
@@ -38,14 +36,6 @@ istream& operator>>(istream& is, KVPair<K,V>& p) {
     return is >> p.key >> colon >> p.value;
 }
 
-// HashTable<Trait>.
-// El Trait debe proveer:
-//   - Trait::Key, Trait::Value
-//   - Trait::value_type  (== KVPair<Key, Value>)
-//   - Trait::Storage     (estructura subyacente — el demo decide cual)
-//
-// El header no menciona AVL, LinkedList, ni ningun tipo concreto. Todo
-// llega via el Trait.
 template <typename Trait>
 class HashTable : public Trait::Storage {
 public:
@@ -104,9 +94,5 @@ public:
         this->internal_insert_unsafe(this->m_pRoot, newPair, Ref{}, nullptr);
     }
 };
-
-// structured bindings: KVPair tiene campos publicos key, value — C++17
-// destructura via Form C (orden de declaracion). Sin especializaciones de
-// tuple_size/tuple_element/get<I> — son redundantes.
 
 #endif // __HASHTABLE_H__
